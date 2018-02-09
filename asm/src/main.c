dbcd312c12b63d3d1b011d78d46a1a403de17064
Gregoire Mordelet <gmordele@student.42.fr>
2018-02-09 14:54:33 +0100

    first commit

---
+++ asm/src/main.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
