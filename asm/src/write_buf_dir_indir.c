e22c161d0e5095e1a2ce72db6fa131e3731a1718
Gregoire Mordelet <gmordele@student.42.fr>
2018-02-15 18:46:56 +0100

    asm: up write_buf_dir_indir.c

--- asm/src/write_buf_dir_indir.c
+++ asm/src/write_buf_dir_indir.c
@@ -6,7 +6,7 @@
 /*   By: gmordele <gmordele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 17:13:19 by gmordele          #+#    #+#             */
-/*   Updated: 2018/02/15 18:25:37 by gmordele         ###   ########.fr       */
+/*   Updated: 2018/02/15 18:40:03 by gmordele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,9 @@ void		write_buf_dir_indir(int *i, int j, t_instruction instruction)
 	else
 	{
 		int_val = int_value(instruction.param[j].value);
-		instruction.write_buf[(*i)++] = ((char *)&(int_value))[3];
-		instruction.write_buf[(*i)++] = ((char *)&(int_value))[2];
-		instruction.write_buf[(*i)++] = ((char *)&(int_value))[1];
-		instruction.write_buf[(*i)++] = ((char *)&(int_value))[0];
+		instruction.write_buf[(*i)++] = ((char *)&(int_val))[3];
+		instruction.write_buf[(*i)++] = ((char *)&(int_val))[2];
+		instruction.write_buf[(*i)++] = ((char *)&(int_val))[1];
+		instruction.write_buf[(*i)++] = ((char *)&(int_val))[0];
 	}
 }
