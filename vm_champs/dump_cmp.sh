2672a56b411cb4a9e2b5d9ec1236220da8bf98b4
Eric DEBISE <edebise@e1r4p16.42.fr>
2018-02-26 19:55:28 +0100

    modif dump_cmp.sh

--- vm_champs/dump_cmp.sh
+++ vm_champs/dump_cmp.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]
-then
+	then
 	echo "usage: \x1b[33mPrécisez un dump, un pas puis au moins un champion"
 	exit
 elif [ -f $1 ] || [ -f $2 ]
-then
+	then
 	echo "usage: \x1b[33mPrecisez un dump de départ et un pas d'incrémentation"
 	exit
 else
@@ -18,8 +18,12 @@ else
 		../corewar/corewar -d $dump $3 $4 $5 $6 > dump_team.txt
 		df=`diff dump_zaz.txt dump_team.txt`
 		if [ "$df" != "" ]
-		then
+			then
 			echo "\x1b[33m" $dump "\x1b[39m\n" "$df"
+			exit
+		elif [ $((dump % ($step * 10))) -eq 0 ]
+			then
+			echo "\x1b[33m" $dump "\x1b[39m"
 		fi
 		dump=$(($dump+$step))
 	done
