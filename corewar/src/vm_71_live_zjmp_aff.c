6fede2a47dc1be3170f4d775095ce826f500832b
Eric DEBISE <edebise@e1r1p6.42.fr>
2018-02-28 20:11:35 +0100

    zjmp % IDX_MOD

---
+++ corewar/src/vm_71_live_zjmp_aff.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm_live_zjmp_aff.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edebise <edebise@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/21 20:37:20 by edebise           #+#    #+#             */
+/*   Updated: 2018/02/27 21:30:22 by proso            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vm_0.h"
+
+/*
+**	vm_live()		code:1
+**	increase process->nb_live by 1
+**	if process->value[0] is a real all->champ[].nb
+**		define all->last_live at n (all->champ[n])
+**	process->step is increased by 1 + DIR_SIZE (so 5)
+*/
+
+void	vm_live(t_all *all, t_process *process)
+{
+	int	n;
+
+	if (vm_check_and_get_args(all, process, 1))
+	{
+		++process->nb_live;
+		visu_print(all, "made a live");
+		n = all->nb_champ;
+		while (n-- > 0)
+			if (process->value[0] == all->champ[n].nb)
+			{
+				all->last_live = n;
+				++all->nb_live;
+				visu_print(all, " from %08x", process->value[0]);
+			}
+		visu_print(all, "\n");
+	}
+	else
+		visu_print(all, "can't live !\n");
+	process->step += process->arg_size[0];
+}
+
+/*
+**	vm_zjmp()		code:9
+**	the process pc jump to the address in the only one param
+**	zjmp works only if the carry is not null
+*/
+
+void	vm_zjmp(t_all *all, t_process *pro)
+{
+	if (vm_check_and_get_args(all, pro, 9) && pro->carry)
+	{
+		pro->pc = vm_ajust_addr(pro->pc + (pro->arg[0] % IDX_MOD));
+		pro->step = 0;
+		visu_print(all, "zjmp to %04x\n", pro->pc);
+	}
+	else
+	{
+		visu_print(all, "can't zjmp !\n");
+		pro->step += pro->arg_size[0];
+	}
+}
+
+/*
+**	vm_aff()		code:16
+**	print a char
+**	this char is define by a register in the only one param
+*/
+
+void	vm_aff(t_all *all, t_process *process)
+{
+	char *str;
+
+	if (vm_check_and_get_args(all, process, 16))
+	{
+		str = (char*)vm_malloc(all, all->aff_str_size + 2);
+		ft_memcpy(str, all->aff_str, all->aff_str_size);
+		str[all->aff_str_size++] = (char)process->value[0];
+		ft_free(1, &all->aff_str);
+		all->aff_str = str;
+		visu_print(all, "aff > %s\n", all->aff_str);
+	}
+	else
+		visu_print(all, "can't aff !\n");
+	process->step += 1 + process->arg_size[0];
+}
---
+++ corewar/src/vm_72_ld_lld_ldi_lldi.c
@@ -0,0 +1,134 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm_ld_lld_ldi_lldi.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edebise <edebise@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/22 12:28:17 by edebise           #+#    #+#             */
+/*   Updated: 2018/02/27 21:29:36 by proso            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vm_0.h"
+
+/*
+**	vm_ld()		code:2
+**	load an int value in the register define by the first param
+**	if params are valids
+**		if the second param is an IND
+**			the value will be find in the arena
+**			the address is define by the IND % IDX_MOD
+**		if the second param is a direct
+**			the value is the direct value
+**		process->carry could be fixed
+**	process->step is increased by sum of arg_size
+*/
+
+void	vm_ld(t_all *all, t_process *pro)
+{
+	int	address;
+
+	if (vm_check_and_get_args(all, pro, 2))
+	{
+		if (pro->decoded[0] & T_IND)
+		{
+			address = pro->pc + (pro->arg[0] % IDX_MOD);
+			pro->value[0] = vm_get_mem(all, address, 4);
+		}
+		pro->r[pro->arg[1]] = pro->value[0];
+		pro->carry = (pro->r[pro->arg[1]] ? 0 : 1);
+		visu_print(all, "ld %08x in r%02x\n", pro->value[0], pro->arg[1]);
+	}
+	else
+		visu_print(all, "can't ld !\n");
+	pro->step += 1 + pro->arg_size[0] + pro->arg_size[1];
+}
+
+/*
+**	vm_lld()		code:13
+**	load an int value in the register define by the first param
+**	if params are valids
+**		if the second param is an IND
+**			the value will be find in the arena
+**			the address is define by the IND % IDX_MOD
+**		if the second param is a direct
+**			the value is the direct value
+**		process->carry could be fixed
+**	process->step is increased by sum of arg_size
+*/
+
+void	vm_lld(t_all *all, t_process *pro)
+{
+	int	address;
+
+	if (vm_check_and_get_args(all, pro, 13))
+	{
+		if (pro->decoded[0] & T_IND)
+		{
+			address = pro->pc + pro->arg[0];
+			pro->value[0] = vm_get_mem(all, address, 2);
+		}
+		pro->r[pro->arg[1]] = pro->value[0];
+		pro->carry = (pro->r[pro->arg[1]] ? 0 : 1);
+		visu_print(all, "lld %08x in r%02x\n", pro->value[0], pro->arg[1]);
+	}
+	else
+		visu_print(all, "can't lld !\n");
+	pro->step += 1 + pro->arg_size[0] + pro->arg_size[1];
+}
+
+/*
+**	vm_ldi()		code:10
+**	load an int value in the register define by the third param
+**	if params are valids
+**		add first and second param value
+**		this value is treated as an address
+**		the value of the register will be find at this address % IDX_MOD
+**	process->step is increased by sum of arg_size
+*/
+
+void	vm_ldi(t_all *all, t_process *pro)
+{
+	int	address;
+
+	if (vm_check_and_get_args(all, pro, 10))
+	{
+		address = pro->pc + ((pro->value[0] + pro->value[1]) % IDX_MOD);
+		address = vm_ajust_addr(address);
+		pro->r[pro->arg[2]] = vm_get_mem(all, address, 4);
+		visu_print(all, "ldi %08x (from %04x) ", pro->r[pro->arg[2]], address);
+		visu_print(all, "in r%02x\n", pro->arg[2]);
+	}
+	else
+		visu_print(all, "can't ldi !\n");
+	pro->step += 1 + pro->arg_size[0] + pro->arg_size[1] + pro->arg_size[2];
+}
+
+/*
+**	vm_lldi()		code:14
+**	load an int value in the register define by the third param
+**	if params are valids
+**		add first and second param value
+**		this value is treated as an address
+**		the value of the register will be find at this address
+**		process->carry could be fixed
+**	process->step is increased by sum of arg_size
+*/
+
+void	vm_lldi(t_all *all, t_process *pro)
+{
+	int	address;
+
+	if (vm_check_and_get_args(all, pro, 14))
+	{
+		address = vm_ajust_addr(pro->pc + pro->value[0] + pro->value[1]);
+		pro->r[pro->arg[2]] = vm_get_mem(all, address, 4);
+		pro->carry = (pro->r[pro->arg[2]] ? 0 : 1);
+		visu_print(all, "lldi %08x (from %04x) ", pro->r[pro->arg[2]], address);
+		visu_print(all, "in r%02x\n", pro->arg[2]);
+	}
+	else
+		visu_print(all, "can't lldi !\n");
+	pro->step += 1 + pro->arg_size[0] + pro->arg_size[1] + pro->arg_size[2];
+}
