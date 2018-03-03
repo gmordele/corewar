6d2e20b51b8065f0fc995b0b1af1ff8feda174ec
Eric DEBISE <edebise@e1r4p8.42.fr>
2018-03-03 19:35:41 +0100

    modif testcorwar

--- champs/testcorwar.sh
+++ champs/testcorwar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-STEP=1000
-MAX=3000
+STEP=2000
+MAX=5000
 CORDIR=./cor
 COREWAR1=../corewar/corewar
 COREWAR2=../vm_champs/corewar
@@ -9,26 +9,32 @@ COREWAR2=../vm_champs/corewar
 corarr=(`ls $CORDIR/*.cor`)
 arrlen=${#corarr[@]}
 
+rm -f failed.txt
+
 for i in `seq 0 $(($arrlen - 1))`
 do
 	for j in `seq $i $(($arrlen - 1))`
 	do
-		for dump in `seq $STEP $STEP $MAX`
+	#	echo "\x1b[2K\x1b[32m${corarr[$i]}\x1b[0m vs \x1b[36m${corarr[$j]}\x1b[0m\x1b[A"
+		for dump in `seq 1000 $STEP $MAX`
 		do
-			df1=`$COREWAR1 ${corarr[$i]} ${corarr[$j]} -dump $dump`
+			echo "\x1b[2K $dump \x1b[32m${corarr[$i]}\x1b[0m vs \x1b[36m${corarr[$j]}\x1b[0m\x1b[A"
+			df1=`$COREWAR1 ${corarr[$i]} ${corarr[$j]} -d $dump`
 			df2=`$COREWAR2 ${corarr[$i]} ${corarr[$j]} -d $dump`
 			if [ "$df1" != "$df2" ]
 			then
-				echo "${corarr[$i]} ${corarr[$j]} FAIL with dump $dump"
-				echo "$df1"
-				echo "$df2"
-				lines=`echo "$df1" | wc -l`
-				echo $lines
-				if [ $lines -gt 10 ]
-				then
-					echo ${corarr[$i]} ${corarr[$j]} dump = $dump >> failed
-				fi
-				break
+				echo "${corarr[$i]} vs ${corarr[$j]} -d $dump" >> failed.txt
+			#	diff $df1 $df2 >> failed.txt
+#				echo "${corarr[$i]} ${corarr[$j]} FAIL with dump $dump"
+#				echo "$df1"
+#				echo "$df2"
+#				lines=`echo "$df1" | wc -l`
+#				echo $lines
+#				if [ $lines -gt 10 ]
+#				then
+#					echo ${corarr[$i]} ${corarr[$j]} dump = $dump >> failed
+#				fi
+#				break
 			fi
 		done
 	done
